9b7279a18ea6af86ca3ce40e18b7000ba5cbc2e8
Alexandr Ben <Alexandr Ben>
2017-03-09 10:26:41 +0200

    Первый коммит - заготовка проекта

---
+++ NotesManager/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotesManager
+{
+    public partial class Form2 : Form
+    {
+        Form parent;
+        public Form2(Form1 x)
+        {
+            InitializeComponent();
+            this.parent = x;
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            parent.Close();
+        }
+    }
+}
